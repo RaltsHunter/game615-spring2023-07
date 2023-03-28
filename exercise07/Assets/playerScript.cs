4da597362775ccc72d4831ca9f48f04c41cfee30
RaltsHunter <117216708+RaltsHunter@users.noreply.github.com>
2023-03-28 16:16:42 -0500

    Added some further interactions

--- exercise07/Assets/playerScript.cs
+++ exercise07/Assets/playerScript.cs
@@ -8,6 +8,11 @@ public class playerScript : MonoBehaviour
     float bulletCount = 0;
     public TMP_Text bullets;
     float gunCount = 0;
+    public float mSpeed;
+    public float rSpeed;
+    public Rigidbody rb;
+    public float badGuyForce;
+    public float carForce;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +23,11 @@ public class playerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //player controls
+        float vAxis = Input.GetAxis("Vertical");
+        float hAxis = Input.GetAxis("Horizontal");
+        gameObject.transform.Translate(gameObject.transform.forward * Time.deltaTime * mSpeed * vAxis, Space.World);
+        gameObject.transform.Rotate(0, rSpeed * Time.deltaTime * hAxis, 0);
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -48,6 +57,16 @@ public class playerScript : MonoBehaviour
             bullets.text = "Yeah, I'm thinking I'm back";
         }
 
+        //bad guys and cars
+        if (other.CompareTag("car"))
+        {
+            rb.AddForce(gameObject.transform.forward * carForce);
+        }
+
+        if (other.CompareTag("badGuy"))
+        {
+            rb.AddForce(gameObject.transform.forward * badGuyForce);
+        }
     }
 
     }
